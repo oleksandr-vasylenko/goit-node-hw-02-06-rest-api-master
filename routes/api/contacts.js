62f7dc503db79c06cbfc897dde97373dd212e594
oleksandr-vasylenko <oleksandr.vasylenko.ua@gmail.com>
2023-02-21 11:12:14 +0200

    Adds contacts

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,28 @@
 const express = require("express");
 
+const contacts = require("../../models/contacts.json");
+
 const router = express.Router();
 
 router.get("/", async (req, res, next) => {
-  res.json({ message: "template message " });
+  const result = await contacts.listContacts();
+  res.json(result);
 });
 
 router.get("/:contactId", async (req, res, next) => {
-  res.json({ message: "template message" });
+  res.json(contacts[0]);
 });
 
 router.post("/", async (req, res, next) => {
-  res.json({ message: "template message" });
+  res.json(contacts);
 });
 
 router.delete("/:contactId", async (req, res, next) => {
-  res.json({ message: "template message" });
+  res.json(contacts);
 });
 
 router.put("/:contactId", async (req, res, next) => {
-  res.json({ message: "template message" });
+  res.json(contacts);
 });
 
 module.exports = router;
