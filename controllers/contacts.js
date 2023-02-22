90411138cbd3794449bec39fde6678da776b8332
oleksandr-vasylenko <oleksandr.vasylenko.ua@gmail.com>
2023-02-22 20:02:44 +0200

    Adds middleware validateBody

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,12 +1,5 @@
-const Joi = require("joi");
 const contacts = require("../models/contacts");
 const { HttpError, ctrlWrapper } = require("../helpers");
-
-const addSchema = Joi.object({
-  name: Joi.string().required(),
-  email: Joi.string().required(),
-  phone: Joi.string().required(),
-});
 
 const getAll = async (req, res) => {
   const result = await contacts.listContacts();
@@ -24,10 +17,6 @@ const getById = async (req, res) => {
 };
 
 const addNew = async (req, res) => {
-  const { error } = addSchema.validate(req.body);
-  if (error) {
-    throw HttpError(400, "missing required name field");
-  }
   const result = await contacts.addContact(req.body);
   res.status(201).json(result);
 };
@@ -46,10 +35,6 @@ const deletebyId = async (req, res) => {
 };
 
 const updateById = async (req, res) => {
-  const { error } = addSchema.validate(req.body);
-  if (error) {
-    throw HttpError(400, "missing fields");
-  }
   const { contactId } = req.params;
   const result = await contacts.updateContact(contactId, req.body);
 
