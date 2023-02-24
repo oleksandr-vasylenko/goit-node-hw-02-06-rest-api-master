375fe4c95585963ce394ec86c785d29cbf4fb207
oleksandr-vasylenko <oleksandr.vasylenko.ua@gmail.com>
2023-02-24 18:55:19 +0200

    Adds mongoose

--- server.js
+++ server.js
@@ -1,5 +1,21 @@
-const app = require('./app')
+const mongoose = require("mongoose");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const app = require("./app");
+
+const DB_HOST =
+  "mongodb+srv://oleksandrv:RMw0VJfyHodujxh0@cluster0.hh5fiki.mongodb.net/contacts_reader?retryWrites=true&w=majority";
+
+mongoose.set("strictQuery", true);
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(3000);
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
+
+// oleksandrv;
+// RMw0VJfyHodujxh0;
