d01bf37c57f95d3f78710357e579f5377d0033b3
oleksandr-vasylenko <oleksandr.vasylenko.ua@gmail.com>
2023-03-06 13:13:09 +0200

    Adjusts errors

--- models/user.js
+++ models/user.js
@@ -30,7 +30,7 @@ const userSchema = new Schema(
 userSchema.post("save", handleMongooseError);
 
 const registerSchema = Joi.object({
-  name: Joi.string().required(),
+  //   name: Joi.string().required(),
   email: Joi.string().pattern(emailRegexp).required(),
   password: Joi.string().min(6).required(),
 });
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,7 +1,13 @@
 const express = require("express");
 
+const ctrl = require("../../controllers/auth");
+
+const { validateBody } = require("../../middlwares");
+
+const { schemas } = require("../../models/user");
+
 const router = express.Router();
 
-// router.post("/register");
+router.post("/register", validateBody(schemas.registerSchema), ctrl.register);
 
 module.exports = router;
