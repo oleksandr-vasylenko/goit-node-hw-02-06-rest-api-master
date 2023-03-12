65e70424410109fc24c4581d9c709572e0728061
oleksandr-vasylenko <oleksandr.vasylenko.ua@gmail.com>
2023-03-12 13:41:30 +0200

    Adds logout

--- controllers/auth.js
+++ controllers/auth.js
@@ -41,7 +41,7 @@ const login = async (req, res) => {
   };
 
   const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "23h" });
-
+  await User.findByIdAndUpdate(user._id, { token });
   res.json({
     token,
   });
@@ -56,8 +56,18 @@ const getCurrent = async (req, res) => {
   });
 };
 
+const logout = async (req, res) => {
+  const { _id } = req.user;
+  await User.findByIdAndUpdate(_id, { token: "" });
+
+  res.json({
+    message: "Logout success",
+  });
+};
+
 module.exports = {
   register: ctrlWrapper(register),
   login: ctrlWrapper(login),
   getCurrent: ctrlWrapper(getCurrent),
+  logout: ctrlWrapper(logout),
 };
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -16,4 +16,7 @@ router.post("/login", validateBody(schemas.loginSchema), ctrl.login);
 
 router.get("/current", authenticate, ctrl.getCurrent);
 
+// logout
+router.post("/logout", authenticate, ctrl.logout);
+
 module.exports = router;
